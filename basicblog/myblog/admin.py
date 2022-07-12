a698f828889425591acea0336dd83dcc0dcce582
= <steveandrearias@gmail.com>
2022-07-12 12:41:40 -0400

    adding models to admin

--- basicblog/myblog/admin.py
+++ basicblog/myblog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, BlogPost, Comment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(BlogPost)
+admin.site.register(Comment)
